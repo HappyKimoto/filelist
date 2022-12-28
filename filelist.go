145cb62211d2b8297977fd2ea45d832916283319
Kimoto <seiji-sw.kimoto@screen-spe.com>
2022-12-28 15:17:35 -0800

    initial

---
+++ filelist.go
@@ -0,0 +1,99 @@
+package filelist
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+)
+
+func getFileModTime(fp string) int64 {
+	obj, err := os.Stat(fp)
+	if err != nil {
+		panic(err)
+	}
+	return obj.ModTime().Unix()
+}
+
+func sortFilesByModTime(files *[]string) {
+	sort.Slice(*files, func(i, j int) bool {
+		return getFileModTime((*files)[i]) < getFileModTime((*files)[j])
+	})
+}
+
+func getFilesTopOnly(dirin string, files *[]string, re *regexp.Regexp) {
+	// list of file info
+	fileInfos, err := ioutil.ReadDir(dirin)
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range fileInfos {
+		if !f.IsDir() {
+			path := filepath.Join(dirin, f.Name())
+			if re.MatchString(path) {
+				*files = append(*files, path)
+			}
+		}
+	}
+}
+
+func getFilesRecursively(dirin string, files *[]string, re *regexp.Regexp) {
+	err := filepath.Walk(dirin,
+		func(path string, info os.FileInfo, err error) error {
+			// error handling
+			if err != nil {
+				return err
+			}
+			// if file is not a directory and file pattern matches, populate the file list
+			if !info.IsDir() {
+				if re.MatchString(path) {
+					*files = append(*files, path)
+				}
+			}
+			// return okay
+			return nil
+		})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func PopulateFiles(
+	pathin string, // input path either file or directory
+	files *[]string, // pointer to stirng slice of absolute file paths
+	regexpattern string, // regular expression pattern to filter the
+	recursively bool, // whether to search files recursively
+	sortbymodtime bool, // sort files by modified time
+) {
+	// compile regular expression
+	re := regexp.MustCompile(regexpattern)
+
+	// evaulate path input
+	fileInfo, err := os.Stat(pathin)
+	if err != nil {
+		fmt.Printf("ERROR: Path %q does not exist.", pathin)
+		panic(err)
+	} else {
+		fmt.Printf("Path %q is valid.", pathin)
+	}
+
+	// check if input path is a file or directory
+	if !fileInfo.IsDir() {
+		// === File ===
+		*files = append(*files, pathin)
+	} else {
+		// === Directory ===
+		if recursively {
+
+		} else {
+			getFilesTopOnly(pathin, files, re)
+		}
+	}
+
+	// sort files by modified time
+	if sortbymodtime {
+		sortFilesByModTime(files)
+	}
+}
